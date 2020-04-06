826521d64ed9f3a5324e187379bffdf7c57d08ef
Luca Scaringella <lucalstyle@gmail.com>
2020-04-06 02:02:02 +0200

    Update fork to cWS version 1.5.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # z-uws 
 
-*Z-uws is a clone of cWS which is a modified version of the uWebsockets v0.14 with some minor tweaks in C++ code and a complete rewrite of JS code. Z-uws has some changes to be compatible with socketcluster.*
+*Z-uws is a clone of cWS which is a modified version of the uWebsockets v0.14 with some minor tweaks in C++ code and a complete rewrite of JS code.*
 
 <h1 align="center">
   <!-- Logo -->
--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,33 @@
-import { WebSocket as Client} from './cws/client';
-import { WebSocketServer as Server} from './cws/server';
+import * as HTTP from 'http';
+import * as HTTPS from 'https';
 
-export {
-    Server,
-    Client
+export type VerifyClientNext = (verified: boolean, code?: number, message?: string) => void;
+
+export type SocketAddress = {
+  remotePort?: number,
+  remoteAddress?: string,
+  remoteFamily?: string
 };
+
+export type ConnectionInfo = {
+  req: HTTP.IncomingMessage,
+  secure: boolean
+  origin?: string,
+};
+
+export type ServerConfigs = {
+  path?: string,
+  port?: number,
+  host?: string,
+  server?: HTTP.Server | HTTPS.Server,
+  noDelay?: boolean,
+  noServer?: boolean,
+  maxPayload?: number,
+  perMessageDeflate?: boolean | { serverNoContextTakeover: boolean },
+  verifyClient?: (info: ConnectionInfo, next: VerifyClientNext) => void
+};
+
+export { WebSocket } from './client';
+export { WebSocketServer } from './server';
+
+export const secureProtocol: string = 'TLSv1_2_method';
