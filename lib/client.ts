70aa6bf527f2b0d2109aec97eff85b410f304bbd
Luca Scaringella <lucalstyle@gmail.com>
2020-07-07 18:47:51 +0200

    Update types.

--- lib/client.ts
+++ lib/client.ts
@@ -90,8 +90,8 @@ export class WebSocket {
       native[this.socketType].send(this.external, message,
           typeof message === 'string' ? OPCODE_TEXT : OPCODE_BINARY, null, !!compress);
     } else {
-      const err = new Error('Socket not connected');
-      (err as any).code = 'SocketNotConnected';
+      const err = new Error('Socket not connected') as Error & {code?: string};
+      err.code = 'SocketNotConnected';
       throw err;
     }
   }
